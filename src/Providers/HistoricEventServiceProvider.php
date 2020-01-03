f2103790bf371b405664b23808b4b7194bb6e770
rafazingano <rafazingano@gmail.com>
2020-01-03 10:56:09 -0300

    Update all

--- src/Providers/HistoricEventServiceProvider.php
+++ src/Providers/HistoricEventServiceProvider.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace App\Providers;
+namespace ConfrariaWeb\Historic\Providers;
 
 use Illuminate\Foundation\Support\Providers\EventServiceProvider as ServiceProvider;
 use ConfrariaWeb\Historic\Listeners\HistoricListener;
